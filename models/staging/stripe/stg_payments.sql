0d63287e6be03abfbf29111003b211dec09edfa7
Fiyin <fiyinfoluwadanielolatunbosun@gmail.com>
2021-08-21 18:31:32 +0000

    added more tests, materializations etc

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,26 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
+with source_payments as (
+    select * from {{ source('stripe', 'payment') }}
+
+    where _batched_at > (select max(_batched_at) from {{ this }})
+
+)
+
 select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount/100 as amount,
-    created as created_date
+    {{ cents_to_dollar('amount') }} as amount,
+    created as created_date,
+    _batched_at as latest_load_date
 
-from {{ source('stripe', 'payment') }} as payments
+from source_payments as payments
+
+
